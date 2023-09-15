c9712153f0c3792406ce3dafe86137c534c0790a
Rebroboga <Rebroboga10@mail.ru>
2023-09-15 14:27:07 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  autoplay: {
+    delay: 4000,
+  },
+})
